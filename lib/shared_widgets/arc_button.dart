6f9773519545cec200d9a9b2cbc46f6c0e30053a
Bahaaib <bahaasco@gmail.com>
2019-09-04 20:08:00 +0200

    added color to constructor

--- lib/shared_widgets/arc_button.dart
+++ lib/shared_widgets/arc_button.dart
@@ -4,16 +4,18 @@ import 'package:farmtrust_app/custom_paints/arc_painter.dart';
 class ArcButton extends StatelessWidget {
   double startAngle;
   double sweepAngle;
+  Color color;
 
-  ArcButton(double startAngle, double sweepAngle) {
+  ArcButton(double startAngle, double sweepAngle, Color color) {
     this.startAngle = startAngle;
     this.sweepAngle = sweepAngle;
+    this.color = color;
   }
 
   @override
   Widget build(BuildContext context) {
     return CustomPaint(
-      painter: ArcPainter(startAngle, sweepAngle),
+      painter: ArcPainter(startAngle, sweepAngle, color),
       child: Container(
         height: 220,
         width: 220,
