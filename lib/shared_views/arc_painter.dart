1bc0c494d8b208b4099bf46e46c37da2e7ae4231
Bahaaib <bahaasco@gmail.com>
2019-09-04 18:21:05 +0200

    added Arc custom painter

---
+++ lib/shared_views/arc_painter.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as Math;
+
+class ArcPainter extends CustomPainter {
+  double startAngle;
+  double sweepAngle;
+
+  ArcPainter(double startAngle, double sweepAngle) {
+    this.startAngle = startAngle;
+    this.sweepAngle = sweepAngle;
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint paint = Paint()
+      ..color = Colors.green
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 80.0;
+
+    num degToRad(num deg) => deg * (Math.pi / 180.0);
+
+    Path path = Path();
+
+    path.addArc(Rect.fromLTWH(0.0, 0.0, size.width, size.height),
+        degToRad(startAngle), degToRad(sweepAngle));
+    canvas.drawShadow(path, Colors.green, 10.0, true);
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
+}
