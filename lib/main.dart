c5241bb906ea7de0845bb2187e91825fb14e302e
Bahaaib <bahaasco@gmail.com>
2019-09-05 21:52:12 +0200

    added appbar with icons

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:farmtrust_app/register_page/register_page.dart';
 import 'package:farmtrust_app/home_page/home_page.dart';
 import 'package:farmtrust_app/reviews_page/reviews_page.dart';
+import 'package:farmtrust_app/review_body_page/review_body_page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final Widget _home = ReviewsPage();
+  final Widget _home = ReviewBodyPage();
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +20,8 @@ class MyApp extends StatelessWidget {
       routes: <String, WidgetBuilder>{
         '/register': (BuildContext context) => RegisterPage(),
         '/home': (BuildContext context) => HomePage(),
-        '/reviews':(BuildContext context) => ReviewsPage()
+        '/reviews': (BuildContext context) => ReviewsPage(),
+        '/review_body': (BuildContext context) => ReviewBodyPage()
       },
     );
   }
