91877fe6454d85874d5b41d1aaa8be598f74494e
Bahaaib <bahaasco@gmail.com>
2019-09-05 21:34:07 +0200

    built reviews card

--- lib/reviews_page/reviews_page_view.dart
+++ lib/reviews_page/reviews_page_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:farmtrust_app/reviews_page/reviews_page_viewmodel.dart';
+import 'package:smooth_star_rating/smooth_star_rating.dart';
 
 class ReviewsPageView extends ReviewsPageViewModel {
   final Color _primaryColor = Color(0xff00c853);
@@ -9,33 +10,110 @@ class ReviewsPageView extends ReviewsPageViewModel {
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(
         SystemUiOverlayStyle(statusBarColor: _primaryColor));
+
     return Scaffold(
-        appBar: PreferredSize(
-            preferredSize: Size.fromHeight(80.0),
-            child: AppBar(
-              elevation: 0.0,
-              backgroundColor: _primaryColor,
-              title: Container(
-                alignment: Alignment.center,
-                margin: EdgeInsets.only(top: 30.0),
-                child: Text('Reviews & Comments'),
-              ),
-              leading: Container(
+      backgroundColor: Colors.grey[200],
+      appBar: PreferredSize(
+          preferredSize: Size.fromHeight(80.0),
+          child: AppBar(
+            elevation: 0.0,
+            backgroundColor: _primaryColor,
+            title: Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.only(top: 30.0),
+              child: Text('Reviews & Comments'),
+            ),
+            leading: Container(
+              alignment: Alignment.topLeft,
+              margin: EdgeInsets.only(top: 20.0),
+              child: IconButton(
+                  icon: Icon(Icons.keyboard_backspace, color: Colors.white),
+                  onPressed: () {}),
+            ),
+            actions: <Widget>[
+              Container(
                 alignment: Alignment.topLeft,
                 margin: EdgeInsets.only(top: 20.0),
                 child: IconButton(
-                    icon: Icon(Icons.keyboard_backspace, color: Colors.white),
+                    icon: Icon(Icons.menu, color: Colors.white),
                     onPressed: () {}),
               ),
-              actions: <Widget>[
+            ],
+          )),
+      body: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (BuildContext context, int position) =>
+              buildListItems(context, position)),
+    );
+  }
+
+  Widget buildListItems(BuildContext context, int position) {
+    return Center(
+      child: Container(
+        margin: EdgeInsets.only(top: 15.0, bottom: 10.0),
+        width: 380.0,
+        height: 90.0,
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(10.0)),
+        child: Row(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(left: 12.0),
+              width: 60.0,
+              height: 60.0,
+              decoration: BoxDecoration(
+                  color: Colors.grey[300], shape: BoxShape.circle),
+              child: Image.asset(
+                'assets/ic_avatar.png',
+                color: Colors.grey[400],
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
                 Container(
-                  alignment: Alignment.topLeft,
-                  margin: EdgeInsets.only(top: 20.0),
-                  child: IconButton(
-                      icon: Icon(Icons.menu, color: Colors.white),
-                      onPressed: () {}),
+                  margin: EdgeInsets.only(top: 20.0, left: 20.0),
+                  child: Text(
+                    'Keria S.',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 10.0, left: 20.0),
+                  child: Text(
+                    'Really Great Experience',
+                    style: TextStyle(color: Colors.grey),
+                  ),
                 ),
               ],
-            )));
+            ),
+            Spacer(
+              flex: 1,
+            ),
+            Column(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(top: 20.0, right: 20.0),
+                  child: SmoothStarRating(
+                    starCount: 5,
+                    borderColor: Colors.orange,
+                    color: Colors.orange,
+                    size: 20.0,
+                    rating: 4,
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 10.0, right: 20.0),
+                  child: Text(
+                    '2 days ago',
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
